306c214b89796cfda3f72bcd42a1f0f16b144444
shibusab <shibusab@gmail.com>
2018-07-04 21:37:19 -0400

    First Commit
    First Commit

---
+++ TestAkka/AkkaConsoleWebApiSelfHosting/Actors/SomeDataEmailActor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestAkka.DummyTestData;
+
+namespace AkkaConsoleWebApiSelfHosting.Actors
+{
+    public class SomeDataEmailActor //: ReceiveActor
+    {
+        public SomeDataEmailActor()
+        {
+            //Receive<SomeDataMessage>(emailData => SaveAndEmail());
+        }
+
+        private void SaveAndEmail()
+        {
+            var emailData = new SomeDataQueryRepository().ReadFromCsvFile();
+
+            Console.WriteLine(string.Format("Received {0} lines ", emailData.Count));
+        }
+    }
+}
---
+++ TestAkka/AkkaConsoleWebApiSelfHosting/DummyTestData/SomeDataQueryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAkka.DummyTestData
+{
+    public class SomeDataQueryRepository
+    {
+        public List<SomeData> ReadFromCsvFile()
+        {
+            List<SomeData> values = File.ReadAllLines(@"DummyTestData\SomeData.csv")
+                                           .Skip(1)
+                                           .Select(v => SomeData.FromCsv(v))
+                                           .ToList();
+            return values;
+        }
+    }
+}
